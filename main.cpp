0faca047368ef866ae40329dbba3e0a1b99e2753
Szymon <32157216+Zawartas@users.noreply.github.com>
2018-02-07 00:05:21 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include <fstream>
 #include <string>
+#include <regex>
 
 int main(int argc, char* argv[])
 {
     std::ifstream input(argv[3]);
     std::ofstream output(argv[4]);
-    std::string line, previous = argv[1], next = argv[2];
+
+    std::regex pattern(argv[1]);
+
+    std::string line;
     while (std::getline(input, line))
-    {
-        for (int index = 0;
-                (index = line.find(previous, index)) != std::string::npos;
-                index += next.size())
-        {
-            line.replace(index, previous.size(), next);
-        }
-        output << line << std::endl;
-    }
+        {output << regex_replace( line, pattern, argv[2]) << std::endl;}
+
     output.close();
     input.close();
     return 0;
